2909f2e9abde12366f71846caa3127fbe2de4d7f
tuukkanisso <48519554+tuukkanisso@users.noreply.github.com>
2019-03-26 10:52:30 +0200

    Create imgSearch.js

---
+++ speedGame/src/js/imgSearch.js
@@ -0,0 +1,16 @@
+import img from '../img/turtsi.jpg';
+import Aava from '../img/Aava.jpg';
+import Anna from '../img/Anna.jpg';
+import target from '../img/target.png';
+import speed from '../img/speed.png';
+import start from '../img/start.png';
+import stop from '../img/stop.png';
+import soundfile from '../audio/AirHorn.mp3';
+import soundSuccess from '../audio/ting.mp3';
+import moiAava from '../audio/moiAava.mp3';
+import moiAnna from '../audio/moiAnna.mp3';
+
+let allFiles={stop:stop,start:start,speed:speed,target:target,img:img,Aava:Aava,Anna:Anna,soundfile:soundfile, soundSuccess:soundSuccess, moiAava:moiAava, moiAnna:moiAnna};
+const users=['aava','anna'];
+
+export  default {allFiles, users};
